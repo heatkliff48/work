8fce4b68af0a127141324802a1b61f686e8336b6
Danila <heatkliff48@gmail.com>
2024-08-28 15:12:49 +0300

    Fix reserv prod

--- work/client/src/components/Warehouse/ListOfReservedProducts/ListOfReservedProductsModal.jsx
+++ work/client/src/components/Warehouse/ListOfReservedProducts/ListOfReservedProductsModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Modal, ModalHeader, ModalBody, Button, Table } from 'reactstrap';
 import { useDispatch } from 'react-redux';
 import { useWarehouseContext } from '#components/contexts/WarehouseContext.js';
@@ -14,11 +14,14 @@ const ListOfReservedProductsModal = React.memo(({ isOpen, toggle }) => {
     warehouseInfoCurIdModal,
     reserveProductModal,
     setReserveProductModal,
+    filteredProducts,
+    setFilteredProducts,
   } = useWarehouseContext();
 
   const { productsOfOrders, list_of_orders } = useOrderContext();
   const { latestProducts } = useProjectContext();
 
+  const [currentListOfResProd, setCurrentListOfResProd] = useState();
   const dispatch = useDispatch();
 
   const curr_warehouse = warehouse_data.find(
@@ -39,12 +42,36 @@ const ListOfReservedProductsModal = React.memo(({ isOpen, toggle }) => {
     );
 
     const order = list_of_orders.find((el) => el.id === orderProductMark?.order_id);
-    return order?.article ?? '';
+    return order?.article;
   };
 
   const deleteHandler = (id) => {
     dispatch(deleteReservedProducts(id));
   };
+
+  useEffect(() => {
+    const curr_res_prod_list = list_of_reserved_products.filter(
+      (el) => el.warehouse_id == curr_warehouse.id
+    );
+    setCurrentListOfResProd(curr_res_prod_list);
+  }, []);
+
+  useEffect(() => {
+    const wh = getWarehouse();
+    const filteredProductsOfOrders = productsOfOrders.filter(
+      (item) => item.product_id === wh.product_id
+    );
+
+    const result = filteredProductsOfOrders.map((product) => {
+      const order = list_of_orders.find((order) => order.id === product.order_id);
+      return {
+        productsOfOrders_id: product.id,
+        order_article: order ? order.article : '',
+        quantity_real: product.quantity_real,
+      };
+    });
+    setFilteredProducts(result);
+  }, [productsOfOrders]);
 
   return (
     <div>
@@ -72,7 +99,9 @@ const ListOfReservedProductsModal = React.memo(({ isOpen, toggle }) => {
           <Button
             style={{ marginBottom: '10px' }}
             color="primary"
-            disabled={curr_warehouse.remaining_stock === 0}
+            disabled={
+              curr_warehouse?.remaining_stock === 0 || filteredProducts?.length === 0
+            }
             onClick={() => {
               setReserveProductModal(!reserveProductModal);
             }}
@@ -88,12 +117,11 @@ const ListOfReservedProductsModal = React.memo(({ isOpen, toggle }) => {
               </tr>
             </thead>
             <tbody>
-              {list_of_reserved_products.map((el) => {
-                const article = getOrderArticle(el?.orders_products_id);
+              {currentListOfResProd?.map((el) => {
                 return (
                   <tr>
-                    <td>{article}</td>
-                    <td>{el.quantity}</td>
+                    <td>{getOrderArticle(el?.orders_products_id)}</td>
+                    <td>{el?.quantity}</td>
                     <td>
                       <Button
                         color="danger"
