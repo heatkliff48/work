b70a604b60ad6ab377665d573007c08dbfbf5e30
libertywalkcat <acheck@list.ru>
2025-06-20 19:20:50 +0300

    new bd migration

---
+++ work/server/db/migrations/20250620191353-create-reserved-dry-mixes.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('ReservedDryMixes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      warehouse_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          key: 'id',
+          model: 'Warehouses',
+        },
+      },
+      orders_products_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          key: 'id',
+          model: 'OrdersProducts',
+        },
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('ReservedProducts');
+  },
+};
