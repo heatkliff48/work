df44a417a4d44c240ff5de181c26775d93cd891c
libertywalkcat <acheck@list.ru>
2025-07-27 21:19:07 +0300

    Offcanvas 100/100

--- work/client/src/components/Main/MainOffcanvas.jsx
+++ work/client/src/components/Main/MainOffcanvas.jsx
@@ -108,7 +108,6 @@ function MainOffcanvas() {
                 Clients
               </Button>
             )}
-
             {checkUserAccess(user, roles, 'Orders')?.canRead && (
               <Button
                 onClick={() => {
@@ -119,7 +118,6 @@ function MainOffcanvas() {
                 Orders
               </Button>
             )}
-
             {checkUserAccess(user, roles, 'List_of_ordered_production')?.canRead && (
               <DropdownButton
                 as={ButtonGroup}
@@ -170,38 +168,138 @@ function MainOffcanvas() {
                 </Dropdown.Item>
               </DropdownButton>
             )}
+            {checkUserAccess(user, roles, 'production_batch_designer')?.canRead && (
+              <DropdownButton
+                as={ButtonGroup}
+                title="Production planner"
+                id="bg-vertical-dropdown-2"
+              >
+                <Dropdown.Item eventKey="1">
+                  <Button>Autoclave calender</Button>
+                </Dropdown.Item>
+                <Dropdown.Item eventKey="2">
+                  {checkUserAccess(user, roles, 'production_batch_designer')
+                    ?.canRead && (
+                    <Button
+                      onClick={() => {
+                        navigate('/production_batch_designer');
+                        handleClose();
+                      }}
+                    >
+                      Batch planner
+                    </Button>
+                  )}
+                </Dropdown.Item>
+              </DropdownButton>
+            )}
+            {checkUserAccess(user, roles, 'production_plan')?.canRead && (
+              <Button
+                onClick={() => {
+                  navigate('/batch_outside');
+                  handleClose();
+                }}
+              >
+                Batch calender
+              </Button>
+            )}
 
-            <DropdownButton
-              as={ButtonGroup}
-              title="Production planner"
-              id="bg-vertical-dropdown-2"
-            >
-              <Dropdown.Item eventKey="1">
-                <Button>Autoclave calender</Button>
-              </Dropdown.Item>
-              <Dropdown.Item eventKey="2">
-                {checkUserAccess(user, roles, 'production_batch_designer')
-                  ?.canRead && (
-                  <Button
-                    onClick={() => {
-                      navigate('/production_batch_designer');
-                      handleClose();
-                    }}
-                  >
-                    Batch planner
-                  </Button>
+            {checkUserAccess(user, roles, 'recipe_products')?.canRead && (
+              <DropdownButton
+                as={ButtonGroup}
+                title="Production planner"
+                id="bg-vertical-dropdown-2"
+              >
+                {checkUserAccess(user, roles, 'recipe_products')?.canRead && (
+                  <Dropdown.Item eventKey="1">
+                    <Button
+                      onClick={() => {
+                        navigate('/recipe_products');
+                        handleClose();
+                      }}
+                    >
+                      Recepies catalog
+                    </Button>
+                  </Dropdown.Item>
                 )}
-              </Dropdown.Item>
-            </DropdownButton>
 
-            <DropdownButton
-              as={ButtonGroup}
-              title="Dropdown"
-              id="bg-vertical-dropdown-3"
-            >
-              <Dropdown.Item eventKey="1">Dropdown link</Dropdown.Item>
-              <Dropdown.Item eventKey="2">Dropdown link</Dropdown.Item>
-            </DropdownButton>
+                {checkUserAccess(user, roles, 'raw_materials_plan')?.canRead && (
+                  <Dropdown.Item eventKey="2">
+                    <Button
+                      onClick={() => {
+                        navigate('/raw_materials_plan');
+                        handleClose();
+                      }}
+                    >
+                      Batch recepie planner
+                    </Button>
+                  </Dropdown.Item>
+                )}
+                {checkUserAccess(user, roles, 'recipe_orders')?.canRead && (
+                  <Dropdown.Item eventKey="3">
+                    <Button
+                      onClick={() => {
+                        navigate('/recipe_orders');
+                        handleClose();
+                      }}
+                    >
+                      Raw material calender
+                    </Button>
+                  </Dropdown.Item>
+                )}
+              </DropdownButton>
+            )}
+            {checkUserAccess(user, roles, 'recipe_orders')?.canRead && (
+              <Button
+                onClick={() => {
+                  navigate('/recipe_orders');
+                  handleClose();
+                }}
+              >
+                Technology calender
+              </Button>
+            )}
+            {checkUserAccess(user, roles, 'quality_management')?.canRead && (
+              <Button
+                onClick={() => {
+                  navigate('/quality_management');
+                  handleClose();
+                }}
+              >
+                Quality management
+              </Button>
+            )}
+
+            {checkUserAccess(user, roles, 'Warehouse')?.canRead && (
+              <Button
+                onClick={() => {
+                  navigate('/warehouse_products_type');
+                  handleClose();
+                }}
+              >
+                Warehouse
+              </Button>
+            )}
+
+            {checkUserAccess(user, roles, 'warehouse_manager')?.canRead && (
+              <Button
+                onClick={() => {
+                  navigate('/warehouse_manager');
+                  handleClose();
+                }}
+              >
+                Order dispatch
+              </Button>
+            )}
+            {checkUserAccess(user, roles, 'accounting')?.canRead && (
+              <Button
+                onClick={() => {
+                  navigate('/accounting');
+                  handleClose();
+                }}
+              >
+                Accounting
+              </Button>
+            )}
           </ButtonGroup>
         </Offcanvas.Body>
       </Offcanvas>
