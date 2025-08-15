a16e0b7f95bfc2370f9c82cc4f2977f23c5fc464
BiggieStonks <161860516+BiggieStonks@users.noreply.github.com>
2025-08-15 12:41:35 +0300

    fixed quantity_free

--- work/client/src/components/ProductionBatchDesigner/ProductionBatchDesigner.jsx
+++ work/client/src/components/ProductionBatchDesigner/ProductionBatchDesigner.jsx
@@ -284,8 +284,14 @@ function ProductionBatchDesigner() {
       const group = groupedByArticle[densityKey];
       group.forEach(({ id, quantity, product_article, quantity_in_warehouse }) => {
         const product = latestProducts.find((el) => el.article == product_article);
-        const { volumeBlockOnPallet, normOfBrack, width, article, m3InArray } =
-          product;
+        const {
+          volumeBlockOnPallet,
+          normOfBrack,
+          width,
+          article,
+          m3InArray,
+          density,
+        } = product;
 
         if (updatedTotalQuantity + quantity <= MAX_QUANTITY) {
           const rightQuantity = quantity - quantity_in_warehouse;
@@ -295,6 +301,7 @@ function ProductionBatchDesigner() {
             id,
             product_article,
             width,
+            density,
             quantity: rightQuantity,
             product_with_brack: ((quantity_m3 + normOfBrack) / m3InArray).toFixed(2),
             quantity_m3,
@@ -502,10 +509,6 @@ function ProductionBatchDesigner() {
 
       return rows;
     });
-  }, [productionBatchDesigner]);
-
-  useEffect(() => {
-    console.log('productionBatchDesigner', productionBatchDesigner);
   }, [productionBatchDesigner]);
 
   return (
