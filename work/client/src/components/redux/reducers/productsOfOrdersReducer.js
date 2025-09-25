189f6694dbf46ac9e4dd8aa69e2ff6c0b7de67f8
Danila <heatkliff48@gmail.com>
2025-09-25 15:43:42 +0300

    change producuts of oder redcer logic

--- work/client/src/components/redux/reducers/productsOfOrdersReducer.js
+++ work/client/src/components/redux/reducers/productsOfOrdersReducer.js
@@ -42,11 +42,14 @@ export const productsOfOrdersReducer = (productsOfOrders = [], action) => {
     }
 
     case UPDATE_PRODUCT_OF_ORDER_REDUCER_SOCKET: {
-      if (Array.isArray(payload)) {
-        return [...productsOfOrders, ...payload];
-      } else {
-        return [...productsOfOrders, payload];
-      }
+      const { productOfOrder } = payload;
+
+      const result = productsOfOrders.map((el) => {
+        if (el.id == productOfOrder.id) return productOfOrder;
+        return el;
+      });
+
+      return result;
     }
 
     case NEW_RANDOM_PRODUCTS_OF_ORDER_SOCKET: {
