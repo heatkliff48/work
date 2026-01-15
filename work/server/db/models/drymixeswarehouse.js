c51538a2257b18ecf2d382512842321afa4f91e7
Danila <heatkliff48@gmail.com>
2026-01-15 17:52:40 +0300

    fix little mistake in model dry mix

--- work/server/db/models/drymixeswarehouse.js
+++ work/server/db/models/drymixeswarehouse.js
@@ -23,7 +23,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: 'DryMixesWarehousess',
+      modelName: 'DryMixesWarehouses',
     }
   );
   return DryMixesWarehouses;
