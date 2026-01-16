6c9dbcc62455dec0be820a45fcc85ccd5295e0e7
libertywalkcat <acheck@list.ru>
2026-01-16 17:47:34 +0300

    new icons for sidebar

--- work/client/src/components/Main/Main.jsx
+++ work/client/src/components/Main/Main.jsx
@@ -74,10 +74,6 @@ import {
 import '#components/Styles/dashboard.css';
 
 
-
-import '#components/Styles/dashboard.css';
-
-
 function Main() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
