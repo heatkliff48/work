fc31cfa0f79557e506944886ad9bb24526afbda4
BiggieStonks <161860516+BiggieStonks@users.noreply.github.com>
2026-03-04 12:10:04 +0300

    Translate Server

--- work/server/services/Auth.js
+++ work/server/services/Auth.js
@@ -9,24 +9,24 @@ const {
 } = require('../utils/Errors.js');
 const UserRepository = require('../repositories/User.js');
 
-
 class AuthService {
   static async signIn({ email, password }) {
     const userData = await UserRepository.getUserData(email);
 
-    if (!userData) throw new NotFound('Пользователь не найден');
+    if (!userData) throw new NotFound('User not found');
 
     const isPasswordValid = bcrypt.compareSync(password, userData.password);
 
-    if (!isPasswordValid) throw new Unauthorized('Неверный логин или пароль');
+    if (!isPasswordValid)
+      throw new Unauthorized('Invalid username or password');
 
-    return userData
+    return userData;
   }
 
   static async signUp({ username, email, password, role }) {
     const userData = await UserRepository.getUserData(email);
     if (userData) {
-      throw new Conflict('Пользователь с такой почтой уже существует.');
+      throw new Conflict('The user with this email already exists.');
     }
 
     const hashedPassword = bcrypt.hashSync(password, 8);
@@ -38,8 +38,7 @@ class AuthService {
       role,
     });
 
-
-    return user
+    return user;
   }
 }
 
